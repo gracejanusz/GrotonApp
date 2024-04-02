8c9a7cfab6776b658ee850b894ce40d191a7fc8a
gracejanusz <71283980+gracejanusz@users.noreply.github.com>
2024-04-01 21:22:59 -0400

    Committing first time

---
+++ GrotonApp/GrotonApp/ContentView 2.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  OAuth2Authorization
+//
+//  Created by Seth Battis on 3/21/24.
+//
+
+import SwiftUI
+import Keys
+
+struct ContentView: View {
+    
+    @State private var path = NavigationPath()
+    @State private var token: OAuth2.TokenResponse?
+    
+    var body: some View {
+        let keys = Keys.OrgGrotonSwiftExamplesOAuth2AuthorizationKeys()
+        var oauth2 = OAuth2(
+            authURL: URL(string: "https://app.blackbaud.com/oauth/authorize")!,
+            tokenURL: URL(string: "https://oauth2.sky.blackbaud.com/token")!,
+            clientID: keys.clientID,
+            clientSecret: keys.clientSecret,
+            redirectURI: URL(string: keys.redirectURI)!
+        )
+        //check to see if refresh token is stored in memory somewhere
+        //if it is then 
+        
+        if (token == nil) {
+            NavigationStack(path: $path) {
+                VStack {
+                    NavigationLink(
+                        destination: WebView(url: oauth2.getAuthorizationURL(flow: .ClientSecret)),
+                        label: {
+                            HStack {
+                                Image(systemName: "person.badge.key.fill")
+                                Text("Get Sky API Access Token")
+                            }
+                        }
+                    )
+                }.onOpenURL(perform: {url in
+                    oauth2.handleRedirect(url, flow: .ClientSecret) {token, error in
+                        guard error == nil else {
+                            print(error!.localizedDescription)
+                            return
+                        }
+                        self.token = token
+                    }
+                })
+            }
+        } else {
+            //Add show home screen thing.
+            TokenView(token: token!)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrotonApp/Pods/Target Support Files/Pods-GrotonApp/Pods-GrotonApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GrotonApp : NSObject
+@end
+@implementation PodsDummy_Pods_GrotonApp
+@end
