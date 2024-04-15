5d8ef0e76142eba57ac71f421bfa30a69ea613a2
Seth Battis <sbattis@groton.org>
2024-04-15 16:11:21 -0400

    Compiles... but gets API error
    1. Updated `ContentView` to let `apiManager` manage the token (which lets it handle redirects appropriately and update the state of the view).
    2. Updated `ScheduleView` to conform to the `View` protocol (which had somehow been missing), and made everything happier again (also added a little cosmetic shine on the error, since I got sidetracked trying to figure out the API error that's being thrown).
    3. Updated the `Schedule` model to have an autogenerated `id` so that it can conform to the `Identifiable` protocol.

--- GrotonApp/ContentView.swift
+++ GrotonApp/ContentView.swift
@@ -23,24 +23,7 @@ struct ContentView: View {
         
         
         
-        if (token == nil) {
-            NavigationStack(path: $path) {
-                VStack {
-                    NavigationLink(
-                        destination: WebView(url: apiManager.getAuthorizationURL(flow: .ClientSecret)),
-                        label: {
-                            HStack {
-                                Image(systemName: "person.badge.key.fill")
-                                Text("Get Sky API Access Token")
-                            }
-                        }
-                    )
-                }.onOpenURL(perform: {url in
-                    apiManager.handleRedirect(url, flow: .ClientSecret)
-                })
-            }
-        } else {
-            
+        if (apiManager.authorized) {
             //https://api.sky.blackbaud.com/school/v1/schedules/{student_id}/meetings?start_date={start_date}[&end_date]
             
             //Add show home screen thing.
@@ -49,13 +32,18 @@ struct ContentView: View {
             //                    //pass in token so scheduleview can call api and use token
             //                    ScheduleView(token: token!)
             //                }
-            //                
+            //
             //                Button("Assignment Center") {
             //                    //change this assignment center view
             //                    ScheduleView(token: token!)
             //                }
             
-            TokenView(token: token!)
+            ScheduleView(studentID: "me")
+        } else {
+            apiManager.authorizationView(flow: .ClientSecret)
+                .onOpenURL() { url in
+                    apiManager.handleRedirect(url, flow: .ClientSecret)
+                }
         }
     }
 }
