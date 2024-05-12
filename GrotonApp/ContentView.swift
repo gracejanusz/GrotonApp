603b1f4096380e07f229e5a97b1d1878e04e2cbf
gracejanusz <71283980+gracejanusz@users.noreply.github.com>
2024-05-12 14:12:37 -0400

    Fixing up schedule view

--- GrotonApp/ContentView.swift
+++ GrotonApp/ContentView.swift
@@ -3,57 +3,8 @@
 //  OAuth2Authorization
 //
 //  Created by Seth Battis on 3/21/24.
-//
-//
-//import SwiftUI
-//import Keys
-//
-//struct ContentView: View {
-//
-//    @State private var path = NavigationPath()
-//    @State private var token: OAuth2.TokenResponse?
-//    @Environment(\.modelContext) private var context
-//    @Environment(APIManager.self) private var apiManager: APIManager
-//
-//
-//
-//    var body: some View {
-//        //check to see if refresh token is stored in memory somewhere
-//        //if it is then
-//        // can I add
-//
-//
-//
-//        if (apiManager.authorized) {
-//            //https://api.sky.blackbaud.com/school/v1/schedules/{student_id}/meetings?start_date={start_date}[&end_date]
-//
-//            //Add show home screen thing.
-//            //            VStack{
-//            //                Button("Schedule"){
-//            //                    //pass in token so scheduleview can call api and use token
-//            //                    ScheduleView(token: token!)
-//            //                }
-//            //
-//            //                Button("Assignment Center") {
-//            //                    //change this assignment center view
-//            //                    ScheduleView(token: token!)
-//            //                }
-//
-//            ScheduleView(user: user)
-//        } else {
-//            apiManager.authorizationView(flow: .ClientSecret)
-//                .onOpenURL() { url in
-//                    apiManager.handleRedirect(url, flow: .ClientSecret)
-//                }
-//        }
-//    }
-//}
-//
-//
-//#Preview {
-//    ContentView()
-//}
 import SwiftUI
+import Keys
 
 struct ContentView: View {
     
@@ -123,3 +74,53 @@ struct ContentView: View {
         }
     }
 }
+
+
+//import SwiftUI
+//import Keys
+//
+//struct ContentView: View {
+//
+//    @State private var path = NavigationPath()
+//    @State private var token: OAuth2.TokenResponse?
+//    @Environment(\.modelContext) private var context
+//    @Environment(APIManager.self) private var apiManager: APIManager
+//
+//
+//
+//    var body: some View {
+//        //check to see if refresh token is stored in memory somewhere
+//        //if it is then
+//        // can I add
+//
+//
+//
+//        if (apiManager.authorized) {
+//            //https://api.sky.blackbaud.com/school/v1/schedules/{student_id}/meetings?start_date={start_date}[&end_date]
+//
+//            //Add show home screen thing.
+//            //            VStack{
+//            //                Button("Schedule"){
+//            //                    //pass in token so scheduleview can call api and use token
+//            //                    ScheduleView(token: token!)
+//            //                }
+//            //
+//            //                Button("Assignment Center") {
+//            //                    //change this assignment center view
+//            //                    ScheduleView(token: token!)
+//            //                }
+//
+//            ScheduleView(user: user)
+//        } else {
+//            apiManager.authorizationView(flow: .ClientSecret)
+//                .onOpenURL() { url in
+//                    apiManager.handleRedirect(url, flow: .ClientSecret)
+//                }
+//        }
+//    }
+//}
+//
+//
+//#Preview {
+//    ContentView()
+//}
